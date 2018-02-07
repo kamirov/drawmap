a5865d2dda78fcf08fdc5f6941b2b0f8eb601284
andrei <andrei@sampler.io>
2018-02-06 21:03:37 -0500

    Add zoom, style, and coords

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  lat: number = 43.6532;
+  lng: number = -79.3832;
+  zoom: number = 14;
+
   constructor() { }
 
   ngOnInit() {
